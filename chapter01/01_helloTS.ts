acc8608c7f5c2ff1da84371125484b51f449382f
陈正清macbook pro <tenchenzhengqing@qq.com>
2023-06-07 15:29:37 +0800

    了解了TypeScript是JavaScript的超集，并且安装了tsc编译器并编译了第一个程序

---
+++ chapter01/01_helloTS.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: czqczqzzzzzz(czq)
+ * @Email: tenchenzhengqing@qq.com
+ * @Date: 2023-06-07 15:16:03
+ * @LastEditors: 陈正清macbook pro
+ * @LastEditTime: 2023-06-07 15:17:38
+ * @FilePath: /尚硅谷TypeScript/chapter01/01_helloTS.ts
+ * @Description: 第一个typescript程序
+ * 
+ * Copyright (c) by czqczqzzzzzz(czq), All Rights Reserved. 
+ */
+console.log('Hello TS');
