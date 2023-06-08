c289e14e56c9239d14f1167e58a694b3de4e4e4c
陈正清MacPro <tenchenzhengqing@qq.com>
2023-06-08 16:48:51 +0800

    介绍了TS中的编译选项，介绍了自动监测编译以及tsconfig中的编译配置 待续

---
+++ chapter01/part2/app.ts
@@ -0,0 +1,15 @@
+/*
+ * @Author: czqczqzzzzzz(czq)
+ * @Email: tenchenzhengqing@qq.com
+ * @Date: 2023-06-08 16:01:36
+ * @LastEditors: 陈正清MacPro
+ * @LastEditTime: 2023-06-08 16:15:03
+ * @FilePath: /atguigu-TypeScript/chapter01/part2/app.ts
+ * @Description: TS中的编译配置
+ * 
+ * Copyright (c) by czqczqzzzzzz(czq), All Rights Reserved. 
+ */
+console.log('hello');
+let a = 10
+let b = 33
+let c = 'hello'
---
+++ chapter01/part2/src/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: czqczqzzzzzz(czq)
+ * @Email: tenchenzhengqing@qq.com
+ * @Date: 2023-06-08 16:28:26
+ * @LastEditors: 陈正清MacPro
+ * @LastEditTime: 2023-06-08 16:28:31
+ * @FilePath: /atguigu-TypeScript/chapter01/part2/src/index.ts
+ * @Description: 
+ * 
+ * Copyright (c) by czqczqzzzzzz(czq), All Rights Reserved. 
+ */
+console.log('aaa');
