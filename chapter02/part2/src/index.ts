b87dedf1e1304fea77eb2b906b90464b3098a76a
陈正清macbook pro <tenchenzhengqing@qq.com>
2023-06-12 17:13:20 +0800

    完成了贪吃蛇项目搭建，并且引入了css-loader、postcss

---
+++ chapter02/part2/src/index.ts
@@ -0,0 +1,15 @@
+/*
+ * @Author: czqczqzzzzzz(czq)
+ * @Email: tenchenzhengqing@qq.com
+ * @Date: 2023-06-12 16:20:10
+ * @LastEditors: 陈正清macbook pro
+ * @LastEditTime: 2023-06-12 16:37:46
+ * @FilePath: /尚硅谷TypeScript/chapter02/part2/src/index.ts
+ * @Description: 项目入口文件
+ * 
+ * Copyright (c) by czqczqzzzzzz(czq), All Rights Reserved. 
+ */
+
+// 引入样式
+import './style/index.less'
+console.log(123);
